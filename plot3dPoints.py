e726c067b0b5c9b1c93e5e6fd997c93b165c437f
Kentaro Barnes <kentaro.barnes5@gmail.com>
2025-07-30 14:00:23 -0700

    3d plot

--- plot3dPoints.py
+++ plot3dPoints.py
@@ -1,3 +1,6 @@
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+
 x = [15.5, 15.8, 15.8, 15.8, 15.3, 15.3, 16.8, 16.8, 19.4, 18.4, 14.5, 14.6, 19, 17.4, 16, 17.1, 18, 16.2, 15.5, 17, 16, 18, 14.7, 17, 17.8, 16, 7.9, 15.1, 15.6, 17.8, 18, 16, 16.9, 17.2, 16.7]
 y = [10.4, 8.8, 6.1, 5.3, 0, 18, 5.7, 10.4, 7.4, 16.4, 8, 10, 8.8, 13.7, 5.7, 6.8, 12.3, 7.8, 8.6, 7.3, 5, 1.4, 12.3, 8, 4.4, 9, 12, 2.8, 5.6, 8.4, 16.6, 7.6, 5, 1.8, 4.3]
 z = [7, 19.4, 16.4, 7.2, 0, 6.1, 8, 21.3, 5.3, 10.2, 13.5, 14, 5.9, 11.3, 15.8, 7.8, 7.2, 12.6, 9.7, 7.1, 11.2, 2, 22.3, 5, 6.9, 5.1, 8.7, 5.8, 9.8, 5.1, 7.4, 12.5, 4.3, 4.3, 4.3]
@@ -18,5 +21,35 @@ for i in range(len(x)):
     else:
         red_points.append(coord)
         
-print(f"Green Points: {green_points}")
-print(f"Red Points: {red_points}")
+plot = plt.figure(figsize=(10,8))
+ax = plot.add_subplot(111, projection="3d")
+
+# split green coords
+green_x = []
+green_y = []
+green_z = []
+for coord in green_points:
+    green_x.append(coord[0])
+    green_y.append(coord[1])
+    green_z.append(coord[2])
+
+#split red coords
+red_x = []
+red_y = []
+red_z = []
+for coord in red_points:
+    red_x.append(coord[0])
+    red_y.append(coord[1])
+    red_z.append(coord[2])
+
+ax.scatter(green_x, green_y, green_z, color='green', label='Soluble')
+ax.scatter(red_x, red_y, red_z, color='red', label='Insoluble')
+
+ax.set_xlabel('X')
+ax.set_ylabel('Y')
+# ax.set_zlabel('Z')
+ax.set_title('3D Scatter Plot')
+
+ax.legend()
+
+plt.show()
